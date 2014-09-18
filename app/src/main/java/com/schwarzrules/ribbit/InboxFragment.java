b670957c6911c1578cc17793f1535fa0e436f7b6
wpschwarz <wschwarz@gmail.com>
2014-09-18 13:40:30 -0700

    Done

--- app/src/main/java/com/schwarzrules/ribbit/InboxFragment.java
+++ app/src/main/java/com/schwarzrules/ribbit/InboxFragment.java
@@ -1,18 +1,24 @@
 package com.schwarzrules.ribbit;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -57,13 +63,63 @@ public class InboxFragment extends ListFragment {
                         usernames[i] = message.getString(ParseConstants.KEY_SENDER_NAME);
                         i++;
                     }
+                    if(getListView().getAdapter() == null) {
+                        MessageAdapter adapter = new MessageAdapter(getListView().getContext(), mMessages);
+                        setListAdapter(adapter);
+                    }
+                    else {
+                        ((MessageAdapter)getListView().getAdapter()).refill(mMessages);
 
-                    MessageAdapter adapter = new MessageAdapter(getListView().getContext(), mMessages);
-                    setListAdapter(adapter);
-
+                    }
 
                 }
             }
         });
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        ParseObject message = mMessages.get(position);
+        String messageType = message.getString(ParseConstants.KEY_FILE_TYPE);
+        ParseFile file = message.getParseFile(ParseConstants.KEY_FILE);
+        Uri fileUri = Uri.parse(file.getUrl());
+
+        if(messageType.equals(ParseConstants.TYPE_IMAGE)) {
+            //View image
+            Intent intent = new Intent(getActivity(), ViewImageActivity.class);
+            intent.setData(fileUri);
+            startActivity(intent);
+        }
+        else {
+            //View video
+
+            Intent intent= new Intent(Intent.ACTION_VIEW, fileUri);
+            intent.setDataAndType(fileUri, "video/*");
+            startActivity(intent);
+        }
+
+        //Delete the message
+
+        List<String> ids = message.getList(ParseConstants.KEY_RECIPIENT_IDS);
+
+        if(ids.size() == 1) {
+            //last recipient, delete file
+            message.deleteInBackground();
+
+        }
+        else {
+            ids.remove(ParseUser.getCurrentUser().getObjectId());
+            ArrayList<String> idsToRemove = new ArrayList<String>();
+            idsToRemove.add(ParseUser.getCurrentUser().getObjectId());
+
+            message.removeAll(ParseConstants.KEY_RECIPIENT_IDS,idsToRemove);
+            message.saveInBackground();
+
+        }
+
+
+
     }
 }
